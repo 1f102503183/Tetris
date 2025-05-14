dc99c09102472fcc98f3bb220772e696cc37a42e
1f102503183 <s1f102503183@iniad.org>
2025-05-14 23:56:45 +0900

    setInterval関数を使う、呼び出した関数のfor文により一瞬で落ちてしまう

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -28,13 +28,33 @@ export default function Home() {
   ]);
   const newBoard = structuredClone(board);
 
+  //ミノを落とす関数
+  const falling = () => {
+    for (let y = 0; y < newBoard.length - 1; y++) {
+      for (let x = 0; x < newBoard[y].length; x++) {
+        if (newBoard[y][x] === 3) {
+          if (newBoard[y + 1][x] === 0 && newBoard[y + 1][x] !== undefined) {
+            newBoard[y][x] = 0;
+            newBoard[y + 1][x] = 3;
+          }
+          setBoard(newBoard);
+        }
+      }
+    }
+  };
+
+  setInterval(falling, 1000);
+
   const clickhandller = (y: number, x: number) => {
     newBoard[y][x] = 1;
+
+    //配列が埋まったらけす
     if (newBoard[y].filter((x) => x === 0).length === 0) {
       for (let i = 0; i < newBoard[y].length; i++) {
         newBoard[y][i] = 0;
       }
     }
+
     setBoard(newBoard);
   };
 
