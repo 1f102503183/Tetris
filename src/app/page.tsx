1a61902a55930dec6a852e64d743fb9673988122
1f102503183 <s1f102503183@iniad.org>
2025-05-15 17:44:13 +0900

    おそらく落とす処理かループに異常がある？

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,39 +24,66 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
   ]);
+  //３が落下中のブロック、１が固定状態のブロック、０が空
   const newBoard = structuredClone(board);
 
-  //ミノを落とす関数
-  const falling = () => {
-    for (let y = 0; y < newBoard.length - 1; y++) {
+  /////////////////////////////////////////////////////////////////////////////////////
+
+  //３を落とす関数
+  const falling = (newBoard: number[][]) => {
+    for (let y = 18; 0 <= y; y--) {
       for (let x = 0; x < newBoard[y].length; x++) {
         if (newBoard[y][x] === 3) {
-          if (newBoard[y + 1][x] === 0 && newBoard[y + 1][x] !== undefined) {
+          if (newBoard[y + 1][x] === 0 || newBoard[y + 1][x] === 1) {
             newBoard[y][x] = 0;
             newBoard[y + 1][x] = 3;
+          } else {
+            newBoard[y][x] = 1;
           }
-          setBoard(newBoard);
         }
       }
     }
   };
 
-  setInterval(falling, 1000);
+  /////////////////////////////////////////////////////////////////////////////////////
 
-  const clickhandller = (y: number, x: number) => {
-    newBoard[y][x] = 1;
-
-    //配列が埋まったらけす
-    if (newBoard[y].filter((x) => x === 0).length === 0) {
-      for (let i = 0; i < newBoard[y].length; i++) {
-        newBoard[y][i] = 0;
+  //配列が埋まったらけす関数
+  const deleteline = (newBoard: number[][]) => {
+    for (let y = newBoard.length - 1; 0 <= y; y--) {
+      if (newBoard[y].filter((n) => n === 0).length === 0) {
+        if (y === 19) {
+          newBoard[y].fill(0);
+        } else {
+          for (let x = 0; x < newBoard[y].length; x++) {
+            newBoard[y][x] = newBoard[y - 1][x];
+          }
+        }
       }
     }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////
+
+  //毎秒実行する所
+  const INTERVAL = () => {
+    falling(newBoard);
+    deleteline(newBoard);
 
     setBoard(newBoard);
   };
+
+  setInterval(INTERVAL, 1000);
+
+  /////////////////////////////////////////////////////////////////////////////////////
+
+  const clickhandller = (y: number, x: number) => {
+    newBoard[y][x] = 3;
+    setBoard(newBoard);
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////
 
   return (
     <div className={styles.container}>
