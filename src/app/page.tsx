584859c985bd377970f09657df79e67808dc9cae
1f102503183 <s1f102503183@iniad.org>
2025-07-03 18:56:37 +0900

    ブロックの固定を一括に

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import styles from './page.module.css';
 export default function Home() {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 3, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -17,7 +16,8 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -39,62 +39,72 @@ export default function Home() {
   ];
   //発生地点はboard[1][5]
 
-  const newBoard = structuredClone(board);
-
-  /////////////////////////////////////////////////////////////////////////////////////
-
   //３を落とす関数
-  const falling = (newBoard: number[][]) => {
-    for (let y = 18; 0 <= y; y--) {
+  const falling = (newBoard: number[][]): number[][] => {
+    const resultBoard = structuredClone(newBoard);
+    for (let y = newBoard.length - 2; 0 <= y; y--) {
       for (let x = 0; x < newBoard[y].length; x++) {
         if (newBoard[y][x] === 3) {
           if (newBoard[y + 1][x] === 0) {
             newBoard[y][x] = 0;
             newBoard[y + 1][x] = 3;
-          } else {
-            newBoard[y][x] = 1;
           }
         }
       }
     }
+    return resultBoard;
   };
-
-  /////////////////////////////////////////////////////////////////////////////////////
 
   //配列が埋まったらけす関数
   const deleteline = (newBoard: number[][]) => {
     for (let y = newBoard.length - 1; 0 <= y; y--) {
-      if (newBoard[y].filter((n) => n === 0).length === 0) {
-        if (y === 19) {
-          newBoard[y].fill(0);
-        } else {
-          for (let i = y; 0 <= i; i--) {
-            for (let x = 0; x < newBoard[y].length; x++) {
-              newBoard[y][x] = newBoard[y - 1][x];
-            }
-          }
+      if (newBoard[y].filter((n) => n === 1).length === 10) {
+        for (let i = y; y < 0; i--) {
+          newBoard[y] = newBoard[y - 1];
         }
       }
     }
   };
 
-  /////////////////////////////////////////////////////////////////////////////////////
-
-  //毎秒実行する所
-  const INTERVAL = () => {
-    falling(newBoard);
-    deleteline(newBoard);
-
-    setBoard(newBoard);
+  //したが固定ブロックの３を１に変換（したが空白でなら固定する）
+  const stickBlock = (newBoard: number[][]): number[][] => {
+    const resultBoard: number[][] = structuredClone(newBoard);
+    for (let y = newBoard.length - 1; 0 <= y; y--) {
+      for (let x = 0; x < newBoard.length; x++) {
+        if (newBoard[y][x] === 3 && (y === newBoard.length || newBoard[y + 1][x] === 1)) {
+          for (let i = newBoard.length - 1; 0 <= y; y--) {
+            resultBoard[i] = newBoard[i].map((num) => (num === 3 ? 1 : num));
+          }
+        }
+      }
+    }
+    return resultBoard;
   };
 
-  /////////////////////////////////////////////////////////////////////////////////////
+  const counter = (board: number[][], number: number): number => {
+    let count: number = 0;
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === number) {
+          count++;
+        }
+      }
+    }
+    return count;
+  };
+
+  //インターバルされる関数、なかにプログラムの手順を描いていく
+  const INTERVAL = () => {
+    const newBoard = structuredClone(board);
+    stickBlock(newBoard);
+    falling(newBoard);
+    // deleteline(newBoard);
+    // setBoard(newBoard);
+  };
 
   const clickhandller = (y: number, x: number) => {
-    newBoard[y][x] = 3;
+    console.log('clicked');
   };
-
-  /////////////////////////////////////////////////////////////////////////////////////
 
   return (
     <div className={styles.container}>
